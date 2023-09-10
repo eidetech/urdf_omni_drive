74ad375e2e1ae4dd1b6fef4e92a0a29347cc8d10
eidetech <marcuseide@me.com>
2023-09-10 21:15:29 +0200

    Robot drives around in gazebo using joy input

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -9,38 +9,58 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
 
-
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
 
-
-    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
-
     package_name='urdf_omni_drive'
 
+    # Path to joy config
+    joy_config_path = PathJoinSubstitution(
+        [FindPackageShare(package_name), "config", "joy.yaml"]
+    )
+
+    # Robot State Publisher
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Include the Gazebo launch file, provided by the gazebo_ros package
+    # Gazebo
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
              )
 
-    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
+    # Spawn robot
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'my_bot'],
                         output='screen')
 
+    # Joy node (for PS3 input)
+    joy_node = Node(package='joy',
+             executable='joy_node',
+             name='joy_node',
+             output='screen'
+        )
+    
+    # Teleop node for converting PS3 input to cmd_vel
+    teleop_node = Node(
+            package='teleop_twist_joy',
+            executable='teleop_node',
+            name='teleop_twist_joy_node',
+            output='screen',
+            parameters=[joy_config_path]
+        )
 
-
-    # Launch them all!
+    # Launch all
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        joy_node,
+        teleop_node
     ])
